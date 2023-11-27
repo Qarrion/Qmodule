f0560da26b1bccd51ba26904e810065fbc4fc073
qarrion <qarrion.k@gmail.com>
2023-11-27 23:23:10 +0900

    feature color = head, level

--- Qlogger/Qlogger/logger.py
+++ Qlogger/Qlogger/logger.py
@@ -26,12 +26,12 @@ class Logger(logging.Logger):
 
     # ------------------------------------------------------------------------ #
     def __init__(self, name:str, 
-        color:Literal['level','red','green','yellow','blue']=None,
+        color:Literal['level','head','red','green','yellow','blue']=None,
         config:str = "log.ini", debug=False):
         """
         #### name : name is section of config /else/ default
         #### color : 'red','green','yellow','blue','purple','cyan','white'
-        #### color : 'level', None
+        #### color : 'level', 'head', None
         #### config : project/config/<log.ini> /else/ defualt
         """
         super().__init__(name, level=logging.NOTSET)
@@ -50,10 +50,11 @@ class Logger(logging.Logger):
         
         # ----------------------------- formatter ---------------------------- #
         if color == 'level':
-            formatter = _LevelFormatter(fmt=log_fmt, datefmt=log_ymd)
+            formatter = LevelFormatter(fmt=log_fmt, datefmt=log_ymd)
+        elif color == 'head':
+            formatter = HeadFormatter(fmt=log_fmt, datefmt=log_ymd)
         else:
             if color is None : color = 'reset'
-
             log_fmt_col = f"{cmap[color]}{log_fmt}{cmap['reset']}"
             formatter = logging.Formatter(fmt=log_fmt_col, datefmt=log_ymd)
     
@@ -65,13 +66,29 @@ class Logger(logging.Logger):
         if self.hasHandlers(): self.handlers.clear()        
         self.addHandler(stream_handler)  
 
-class _LevelFormatter(logging.Formatter):
+class LevelFormatter(logging.Formatter):
+    def __init__(self, fmt=None, datefmt=None, style='%'):
+        super().__init__(fmt, datefmt, style)
+
     def format(self, record):
-        levelname = record.levelname
-        if levelname in level:
-            levelname_color = level[levelname] + levelname + level['RESET']
-            record.levelname = levelname_color
-        return logging.Formatter.format(self, record)
+        original_formatted_log = super().format(record)
+
+        level_color = level.get(record.levelname, cmap['reset'])
+        colored_log = f"{level_color}{original_formatted_log}{cmap['reset']}"
+        return colored_log
+    
+class HeadFormatter(logging.Formatter):
+    def __init__(self, fmt=None, datefmt=None, style='%'):
+        super().__init__(fmt, datefmt, style)
+
+    def format(self, record):
+        original_formatted_log = super().format(record)
+
+        level_color = level.get(record.levelname, cmap['reset'])
+        levelname_with_color = f"{level_color}{record.levelname}{cmap['reset']}"
+
+        colored_log = original_formatted_log.replace(record.levelname, levelname_with_color)
+        return colored_log
 
 class CustomLog:
     def __init__(self, logger:logging.Logger,
@@ -147,25 +164,32 @@ class _Async:
         self._level.set(value)
 
 if __name__ == "__main__":
+    print('# ---------------------------------- fix --------------------------------- #')
     logger = Logger('test_fix_logger', 'blue', 'log.ini', True)
     logger.info('info')
     logger.debug('debug')
     logger.warning('warn')
     logger.error('error')
-
-    logger = Logger('test_lev_logger', 'level', 'log.ini', True)
+    print('# --------------------------------- level -------------------------------- #')
+    logger = Logger('test_level_logger', 'level', 'log.ini', True)
     logger.info('info')
     logger.debug('debug')
     logger.warning('warn')
     logger.error('error')
-
+    print('# --------------------------------- head --------------------------------- #')
+    logger = Logger('test_head_logger', 'head', 'log.ini', True)
+    logger.info('info')
+    logger.debug('debug')
+    logger.warning('warn')
+    logger.error('error')
+    print('# --------------------------------- green -------------------------------- #')
     logger = Logger('test_chain', 'green', 'log.ini', False)
     chain = CustomLog(logger, 'async')
     chain.info.msg('info')
     chain.debug.msg('debug')
     chain.warning.msg('warning')
     chain.error.msg('error')
-
+    print('# ------------------------------- customlog ------------------------------ #')
     class Log(CustomLog):
         def custom_msg(self, module, status, msg):
             header=f":: {module:<10} {status:<10}"
