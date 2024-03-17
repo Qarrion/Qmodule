668e9a79aca4cbff62db95256c97c74d54773a60
qarrion <qarrion.k@gmail.com>
2024-03-17 22:42:17 +0900

    chore

--- Qlimiter/main.py
+++ Qlimiter/main.py
@@ -1,6 +1,25 @@
-# main
+from Qlimiter.asyncio import Limiter
+from Qlogger import Logger
 
-from Qlimiter.Thread import Limiter
 
-limiter = Limiter(3, 1, 'outflow')
+if __name__ == "__main__":
+    import asyncio
 
+    logger = Logger('test', 'head', debug=False)
+    logger._dev_stream_handler_level('INFO')
+    limiter = Limiter(3, 1, 'outflow', logger=logger)
+    async def myfunc1(x):
+        await asyncio.sleep(x)
+
+    async def myfunc2(x):
+        raise Exception("raise Error")
+
+    async def main():
+        limiter.register(myfunc1)
+        limiter.register(myfunc2)
+
+        await limiter.enqueue('myfunc1',(1,))
+        await limiter.enqueue('myfunc2',(2,))
+        await limiter.taskgroup()
+
+    asyncio.run(main())
