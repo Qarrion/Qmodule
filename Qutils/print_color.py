319d147a6379f2a4f08229b19832305b07e7e2e8
qarrion <qarrion.k@gmail.com>
2024-06-22 22:19:30 +0900

    print color

--- Qutils/print_color.py
+++ Qutils/print_color.py
@@ -5,19 +5,34 @@ from typing import Literal
 # "\033[32m  \033[0m"
 # "\033[31m  \033[0m"
 
-def cprint(msg, color:Literal['red','green']):
-    name = ['green', 'red', 'reset']
-    shell = "\033[32m", "\033[31m", "\033[0m"
-    cmap = dict(zip(name, shell))
+cmap = dict(
+	reset = "\033[0m",
+	red = "\033[31m",
+	green = "\033[32m",
+	yellow = "\033[33m",
+	blue = "\033[34m",
+	purple = "\033[35m",
+	cyan = "\033[36m",
+	white = "\033[37m",
+	
+    red_ = "\033[41m",
+	green_ = "\033[42m",
+	yellow_ = "\033[43m",
+	blue_ = "\033[44m",
+	purple_ = "\033[45m",
+	cyan_ = "\033[46m",
+	white_ = "\033[47m",
+    )
+
+hint = Literal['red','green','yellow','blue','purple','cyan','white','_']
+
+def cprint(msg, color:hint):
     print(f"{cmap[color]}{msg}{cmap['reset']}")
-    if shell=='green':
-        print(f"{cmap[color]}{msg}{cmap['reset']}")
-    elif shell =='red':
-        print(f"{cmap[color]}{msg}{cmap['reset']}")
+
 
 
 
 if __name__ == "__main__":
     cprint('hi','green')
     cprint('hi','red')
-
+    cprint('gi','blue_')
