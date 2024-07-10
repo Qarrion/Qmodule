80d8e414b3afb05dff820b552140bb653602a44c
qarrion <qarrion.k@gmail.com>
2024-07-11 00:58:31 +0900

    AsyncPool

--- Qpgsql/Qpgsql/pgsql.py
+++ Qpgsql/Qpgsql/pgsql.py
@@ -6,6 +6,7 @@ from configparser import ConfigParser
 import sys, asyncio
 from typing import Literal
 import psycopg
+from psycopg_pool import AsyncConnectionPool
 from psycopg.rows import namedtuple_row
 
 class Pgsql:
@@ -60,7 +61,6 @@ class Pgsql:
             dbname=self.config.get('connect','database')
         )
         return cnxn
-    
 
     def connect(self):
         """>>> psycopg.connect(self.conn_str)"""
@@ -72,6 +72,11 @@ class Pgsql:
         + await xconn.commit() should be placed in xcurs context"""
         return psycopg.AsyncConnection.connect(self.conn_str)
 
+    def xconnect_pool(self, name='pgsql'):
+        # https://www.psycopg.org/psycopg3/docs/api/pool.html#module-psycopg_pool
+        pool = AsyncConnectionPool(self.conn_str,open=False, name=name)
+        
+        return pool
     # ------------------------------------------------------------------------ #
     #                                   utils                                  #
     # ------------------------------------------------------------------------ #
@@ -97,8 +102,10 @@ class Pgsql:
 
 
 if __name__ == "__main__":
+    import time
     pgsql = Pgsql()
     conn = pgsql.connect()
+    
     # conn_async = pgsql.xconnect()
     # print(conn_async)
     # from Qpgsql.utils.print_divider import eprint
@@ -121,66 +128,126 @@ if __name__ == "__main__":
     # print(pgsql.conn_str)
 
     # ------------------------------------------------------------------------ #
-    async def create():
-        q = """
-        CREATE TABLE tbl_test (
-            id SERIAL PRIMARY KEY,
-            name VARCHAR(100) NOT NULL,
-            age INTEGER NOT NULL
-        )"""
-        async with await pgsql.xconnect() as aconn:
-            async with aconn.cursor() as acur:
-                await acur.execute(q)
+    # async def create():
+    #     q = """
+    #     CREATE TABLE tbl_test (
+    #         id SERIAL PRIMARY KEY,
+    #         name VARCHAR(100) NOT NULL,
+    #         age INTEGER NOT NULL
+    #     )"""
+    #     async with await pgsql.xconnect() as aconn:
+    #         async with aconn.cursor() as acur:
+    #             await acur.execute(q)
     # ------------------------------------------------------------------------ #
-    async def insert():
-        async with await pgsql.xconnect() as aconn:
-            async with aconn.cursor() as acurs:
-                # tbl_test 테이블에 데이터를 삽입하는 SQL 쿼리
-                insert_query = """
-                INSERT INTO tbl_test (name, age) VALUES (%s, %s)
-                """
+    # async def insert():
+    #     async with await pgsql.xconnect() as aconn:
+    #         async with aconn.cursor() as acurs:
+    #             # tbl_test 테이블에 데이터를 삽입하는 SQL 쿼리
+    #             insert_query = """
+    #             INSERT INTO tbl_test (name, age) VALUES (%s, %s)
+    #             """
                 
-                # 삽입할 데이터
-                data_to_insert = [
-                    ("Alice", 30),
-                    ("Bob", 25),
-                    # ("Charlie", 35)
-                ]
+    #             # 삽입할 데이터
+    #             data_to_insert = [
+    #                 ("Alice", 30),
+    #                 ("Bob", 25),
+    #                 # ("Charlie", 35)
+    #             ]
                 
-                # 여러 행 삽입
-                for data in data_to_insert:
-                    await acurs.execute(insert_query, data)
+    #             # 여러 행 삽입
+    #             for data in data_to_insert:
+    #                 await acurs.execute(insert_query, data)
                 
-                await aconn.commit()
-            print('acurs out not commit? no ! if then ::aconn.commit()::' )
-        print('aconn out commit? yes!')
-                # 변경 사항을 커밋합니다.
-                # conn.commit()
-                # print("Data inserted successfully")
+    #             await aconn.commit()
+    #         print('acurs out not commit? no ! if then ::aconn.commit()::' )
+    #     print('aconn out commit? yes!')
+    #             # 변경 사항을 커밋합니다.
+    #             # conn.commit()
+    #             # print("Data inserted successfully")
     # ------------------------------------------------------------------------ #
-    async def main():
-        # await create()     
-        await insert()     
+#     async def main():
+#         # await create()     
+#         await insert()     
         
-    asyncio.run(main())
-"""
-async with await psycopg.AsyncConnection.connect(
-        "dbname=test user=postgres") as aconn:
-    async with aconn.cursor() as acur:
-        await acur.execute(
-            "INSERT INTO test (num, data) VALUES (%s, %s)",
-            (100, "abc'def"))
-        await acur.execute("SELECT * FROM test")
-        await acur.fetchone()
-        # will return (1, 100, "abc'def")
-        async for record in acur:
-            print(record)
-"""
+#     asyncio.run(main())
+# """
+# async with await psycopg.AsyncConnection.connect(
+#         "dbname=test user=postgres") as aconn:
+#     async with aconn.cursor() as acur:
+#         await acur.execute(
+#             "INSERT INTO test (num, data) VALUES (%s, %s)",
+#             (100, "abc'def"))
+#         await acur.execute("SELECT * FROM test")
+#         await acur.fetchone()
+#         # will return (1, 100, "abc'def")
+#         async for record in acur:
+#             print(record)
+# """
 
-"""
-with pgsql.connect() as conn:
-    with conn.cursor() as curs:
-        curs.row_factory = pgsql.namedtuple_row
-        curs.execute("select * from market_list where market = 'KRW-BTC'")
-        rows = curs.fetchall()
-"""
+# """
+# with pgsql.connect() as conn:
+#     with conn.cursor() as curs:
+#         curs.row_factory = pgsql.namedtuple_row
+#         curs.execute("select * from market_list where market = 'KRW-BTC'")
+#         rows = curs.fetchall()
+# """
+# import timeit
+# import time
+# ---------------------------------------------------------------------------- #
+# def main1():
+#     with pgsql.connect() as conn:
+#         with conn.cursor() as curs:
+#             curs.execute("select * from candle_m1 where market='KRW-BTC' and time > '2024-01-01 00:00:00' ")
+#             curs.execute("select * from candle_m1 where market='KRW-BTC' and time > '2024-01-01 00:00:00' ")
+#             curs.execute("select * from candle_m1 where market='KRW-BTC' and time > '2024-01-01 00:00:00' ")
+ 
+#             # print(rows)
+# a = time.time()
+# main1()
+# print(time.time() - a )
+# ---------------------------------------------------------------------------- #
+
+# async def main2():
+#     async with await pgsql.xconnect() as xconn:
+#         async with xconn.cursor() as xcurs:
+#             await xcurs.execute("select * from candle_m1 where market='KRW-BTC' and time > '2024-01-01 00:00:00' ")
+#             await xcurs.execute("select * from candle_m1 where market='KRW-BTC' and time > '2024-01-01 00:00:00' ")
+#             await xcurs.execute("select * from candle_m1 where market='KRW-BTC' and time > '2024-01-01 00:00:00' ")
+ 
+#             # print(rows)
+# a = time.time()
+# asyncio.run(main2())
+# print(time.time() - a )
+
+
+# async def main3():
+#     async with pgsql.xconnect_pool() as xpool:
+#         # xpool.close()
+#         # xpool.closed
+#         async with xpool.connection() as xconn:
+#             async with xconn.cursor() as xcurs:
+#                 await xcurs.execute("select * from candle_m1 where market='KRW-BTC' and time > '2024-01-01 00:00:00' ")
+#                 await xcurs.execute("select * from candle_m1 where market='KRW-BTC' and time > '2024-01-01 00:00:00' ")
+#                 await xcurs.execute("select * from candle_m1 where market='KRW-BTC' and time > '2024-01-01 00:00:00' ")
+ 
+#             # print(rows)
+# a = time.time()
+# asyncio.run(main3())
+# print(time.time() - a )
+
+    async def main4():
+        xpool = pgsql.xconnect_pool()
+        xpool.open()
+        # async with pgsql.xconnect_pool() as xpool:
+            # xpool.close()
+            # xpool.closed
+        async with xpool.connection() as xconn:
+            async with xconn.cursor() as xcurs:
+                await xcurs.execute("select * from candle_m1 where market='KRW-BTC' and time > '2024-01-01 00:00:00' ")
+                await xcurs.execute("select * from candle_m1 where market='KRW-BTC' and time > '2024-01-01 00:00:00' ")
+                await xcurs.execute("select * from candle_m1 where market='KRW-BTC' and time > '2024-01-01 00:00:00' ")
+    
+                # print(rows)
+    a = time.time()
+    asyncio.run(main4())
+    print(time.time() - a )
--- Qpgsql/Qpgsql/tools/hint.py
+++ Qpgsql/Qpgsql/tools/hint.py
@@ -1,3 +1,4 @@
 from psycopg import Cursor
 from psycopg import Connection
 from psycopg import AsyncConnection
+from psycopg_pool import AsyncConnectionPool
